3b91f82d898a3bfde5806c8ebcb17b3373356f91
Lucas <161894435+lucas-l-silva@users.noreply.github.com>
2025-05-01 00:23:40 +0000

    correcao de caminho; debug de bairros.py

--- utils/carga_dados.py
+++ utils/carga_dados.py
@@ -5,7 +5,7 @@ import os
 
 @st.cache_data
 def carregar_bairros_fortaleza():
-    caminho_shp = os.path.join("data", "bairros_fortaleza", "vw_Fortaleza_Bairros.shp")
+    caminho_shp = "/workspaces/GeoVoto/data/bairros_fortaleza/vw_Fortaleza_Bairros.shp"
     gdf = gpd.read_file(caminho_shp)
     if gdf.crs.to_epsg() != 4326:
         gdf = gdf.to_crs(epsg=4326)
